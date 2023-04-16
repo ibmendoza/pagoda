87c11854e8575041bf625a06ebb9ea8a4ec6b42d
Isagani Mendoza <gani.mendoza@gmail.com>
2023-04-17 00:42:11 +0800

    Replace ent with github.com/bokwoon95/sq

--- pkg/routes/login.go
+++ pkg/routes/login.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/mikestefanello/pagoda/ent"
-	"github.com/mikestefanello/pagoda/ent/user"
+	//"github.com/mikestefanello/pagoda/ent"
+	//"github.com/mikestefanello/pagoda/ent/user"
 	"github.com/mikestefanello/pagoda/pkg/context"
 	"github.com/mikestefanello/pagoda/pkg/controller"
 	"github.com/mikestefanello/pagoda/pkg/msg"
 
+	"github.com/bokwoon95/sq"
 	"github.com/labstack/echo/v4"
 )
 
@@ -24,6 +25,12 @@ type (
 		Submission controller.FormSubmission
 	}
 )
+
+type User struct {
+	ID   int
+	Pswd string
+	Name string
+}
 
 func (c *login) Get(ctx echo.Context) error {
 	page := controller.NewPage(ctx)
@@ -63,32 +70,52 @@ func (c *login) Post(ctx echo.Context) error {
 		return c.Get(ctx)
 	}
 
-	// Attempt to load the user
-	u, err := c.Container.ORM.User.
-		Query().
-		Where(user.Email(strings.ToLower(form.Email))).
-		Only(ctx.Request().Context())
-
-	switch err.(type) {
-	case *ent.NotFoundError:
-		return authFailed()
-	case nil:
-	default:
-		return c.Fail(err, "error querying user during login")
+	usr, err := sq.FetchOne(c.Container.Database, sq.
+		Queryf("SELECT {*} FROM users WHERE email = {}", strings.ToLower(form.Email)).
+		SetDialect(sq.DialectPostgres),
+		func(row *sq.Row) User {
+			return User{
+				ID:   row.Int("id"),
+				Pswd: row.String("password"),
+				Name: row.String("name"),
+			}
+		},
+	)
+	if err != nil {
+		return c.Fail(err, "login sq")
 	}
 
+	/*
+		// Attempt to load the user
+		u, err := c.Container.ORM.User.
+			Query().
+			Where(user.Email(strings.ToLower(form.Email))).
+			Only(ctx.Request().Context())
+
+		switch err.(type) {
+		case *ent.NotFoundError:
+			return authFailed()
+		case nil:
+		default:
+			return c.Fail(err, "error querying user during login")
+		}
+	*/
+
 	// Check if the password is correct
-	err = c.Container.Auth.CheckPassword(form.Password, u.Password)
+	//err = c.Container.Auth.CheckPassword(form.Password, u.Password)
+	err = c.Container.Auth.CheckPassword(form.Password, usr.Pswd)
 	if err != nil {
 		return authFailed()
 	}
 
 	// Log the user in
-	err = c.Container.Auth.Login(ctx, u.ID)
+	//err = c.Container.Auth.Login(ctx, u.ID)
+	err = c.Container.Auth.Login(ctx, usr.ID)
 	if err != nil {
 		return c.Fail(err, "unable to log in user")
 	}
 
-	msg.Success(ctx, fmt.Sprintf("Welcome back, <strong>%s</strong>. You are now logged in.", u.Name))
+	//msg.Success(ctx, fmt.Sprintf("Welcome back, <strong>%s</strong>. You are now logged in.", u.Name))
+	msg.Success(ctx, fmt.Sprintf("Welcome back, <strong>%s</strong>. You are now logged in.", usr.Name))
 	return c.Redirect(ctx, "home")
 }
